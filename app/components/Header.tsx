d6900e76ba1d902c4a3b0494c16e00e6fb4752ec
kaveesha wijegurusinghe <155706266+kaveesha4890@users.noreply.github.com>
2025-09-05 08:36:32 +0530

    develop theheader component

---
+++ app/components/Header.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import React from 'react'
+import { HiBell, HiChat, HiSearch } from 'react-icons/hi'
+
+function Header() {
+  return (
+    <div className='flex gap-3 md:gap-3 items-center p-6'>
+      <Image src={'/logo.png'} alt='logo' width={50} height={50} className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/>
+      <button className='bg-black text-white p-2 px-4 rounded-full'>Home</button>
+      <button className='p-2 px-4 rounded-full font-semibold'>Create</button>
+      <div className='bg-[#e9e9e9] p-3 gap-3 items-center rounded-full w-full hidden md:flex'>
+        <HiSearch className='text-[25px] text-gray-500'/>
+        <input type="text" placeholder='Search' className='bg-transparent outline-none' />
+      </div>
+        <HiSearch className='text-[25px] text-gray-500 md:hidden'/>
+        <HiBell className='text-[25px] md:text-[40px] text-gray-500'/>
+        <HiChat className='text-[25px] md:text-[40px] text-gray-500'/>
+        <Image src="/man.png" alt='user-image' width={50} height={50} className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/>
+    </div>
+  )
+}
+
+export default Header
